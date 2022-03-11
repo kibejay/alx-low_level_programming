8027ea4354f0ed67369b85742fb4d2f2baba7d02
kibejay <kibejay61@gmail.com>
2022-03-10 17:45:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
-{ #include <stdio.h>
+#include <stdio.h>
 
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	int main(void)
 
-return (1);
-} }
+{
+       	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+        return (1);
+} 
